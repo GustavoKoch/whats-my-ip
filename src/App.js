5b69762589902921b52ffd9169049cdfd1f12cc1
Gustavo Koch <guzko@hotmail.com>
2022-05-05 20:25:34 +0200

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+
+
 
 function App() {
+
+  const [ipLocation, setIpLocation]=useState();
+
+  useEffect(() => {
+    setIsLoading(true);   
+
+    const urlSearch= new URL("https://geo.ipify.org/api/v2/country,city?apiKey=at_HXLoCImM2y30wTnSnKWjwCPpZRUmQ");
+ 
+   
+     console.log("Hello URL:"+ urlSearch); 
+
+    fetch(urlSearch) 
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `An error has occured while fetching the data. Status code: ${response.status}`
+          );
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setNews(data);
+        console.log(data)
+       
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsError(true);
+        setIsLoading(false);
+      });
+  }, [ipLocation]);
+
+
+  // https://www.npmjs.com/package/react-paginate
+
+  // npm install --save react-spinners
+ 
+  // let array = [];
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
 
 export default App;
+
+
+// 'https://geo.ipify.org/api/v2/country,city?apiKey=at_HXLoCImM2y30wTnSnKWjwCPpZRUmQ'
+
+// 'https://geo.ipify.org/api/v2/country,city?apiKey={process.env.REACT_APP_API_KEY_IPADDRESS}'
